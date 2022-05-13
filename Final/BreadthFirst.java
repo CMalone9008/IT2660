dd642a78b4226d25eb8ee1b6300496cd6803e344
Carl Malone <59892137+CMalone9008@users.noreply.github.com>
2022-05-13 16:28:52 -0400

    Add files via upload

---
+++ Final/BreadthFirst.java
@@ -0,0 +1,22 @@
+package node;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class BreadthFirst {
+
+public BreadthFirst(){    
+}
+public int b(Node node, LinkedList<Node> list){
+  int count = 0;
+Queue <Node> queue = new LinkedList <Node>(); 
+queue.add(node);
+node.setVisitedOnce(true);
+
+while(!queue.isEmpty())
+    
+
+
+
+}
+}
